b8297f349984e2f010ce58eae96916e0bb3fd6f3
Logan Powell <loganp@tepper.cmu.edu>
2020-03-16 14:49:48 -0400

    attempting `<br>` linebreaks

--- scraper.js
+++ scraper.js
@@ -97,7 +97,7 @@ const getContentForPageBySelectors = async ({ pageURL, selectors }) => {
             .textContent.trim()
             .split(/\r?\n/g)
             .slice(0, 3)
-            .join("\r\n"),
+            .join("<br>"),
         }
       : { [tag]: document.querySelector(sel) }
   }
